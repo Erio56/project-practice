9e6cd95dd23328133f38e7d882dc7d83e8f82ae6
Brayan Cataño <brayansanchez67@hotmail.com>
2024-03-13 22:52:07 -0500

    added github actions

--- task #1/index.ts
+++ task #1/index.ts
@@ -1,57 +1,52 @@
-import express, { Express, Request, Response } from 'express';
-import { randomBytes } from 'node:crypto';
+import express, { type Express, type Request, type Response } from 'express'
+import { randomBytes } from 'node:crypto'
 
-import config from './config.json';
+import config from './config.json'
 
-const app:Express = express();
+const app: Express = express()
 const port = config.server_port
 app.use(express.json())
 
-
 interface SecretMessage {
-   message: string
+  message: string
 }
 
 interface SecretKey {
-   secretKey: string
+  secretKey: string
 }
 
-let secrets: Map<string, string> = new Map() //repace with a map
-
+const secrets = new Map<string, string>() // repace with a map
 
 app.post('/api/secrets', (req: Request, res: Response) => {
+  const secretMessage: SecretMessage = req.body
+  const generatedKey: string = randomBytes(config.secret_size).toString('hex')
 
-   const secretMessage: SecretMessage = req.body;
-   const generatedKey: string = randomBytes(config.secret_size).toString('hex');
+  const secretKey: SecretKey = {
+    secretKey: generatedKey
+  }
+  secrets.set(generatedKey, secretMessage.message)
 
-   const secretKey: SecretKey = {
-      secretKey: generatedKey
-   }
-   secrets.set(generatedKey,secretMessage.message);
-
-   res.status(200).json(secretKey);
-});
-
-app.get('/api/secret/:key', (req: Request, res: Response) => {
-
-   const key = req.params.key;
-   let response : SecretMessage = {
-      message: 'Not Found'
-   };
-
-   const secret = secrets.get(key);
-
-   if(!secret){
-      res.status(404).json(response);
-      return;
-   }
-   
-   response.message = secret;
-   res.status(200).json(response);
-  
+  res.status(200).json(secretKey)
 })
 
+app.get('/api/secrets/:key', (req: Request, res: Response) => {
+  const key = req.params.key
+  const response: SecretMessage = {
+    message: 'Not Found'
+  }
+
+  const secret = secrets.get(key)
+
+  if (secret === null || secret === undefined) {
+    res.status(404).json(response)
+    return
+  }
+
+  response.message = secret
+  secrets.delete(key)
+  res.status(200).json(response)
+})
 
 app.listen(port, () => {
-   console.log(`running in port ${port}.`);
+  console.log(`running in port ${port}.`)
 })
